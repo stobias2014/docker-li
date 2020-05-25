a86966f8b889ce436bab1514af17c25842b633fc
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-25 17:06:12 -0500

    added docker-compose

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,30 @@
 import express from 'express';
-import mongodb from 'mongodb';
-import config from './data';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 4000;
-const client = mongodb.MongoClient;
 
-client.connect(config.DB, { useNewUrlParser: true }, (err, db) => { 
-    if(err) {
-        console.log('database is not connected')
-    }
-    else {
-        console.log('connected!!')
-    }
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mongo:27017/crm', {
+    useMongoClient: true
 });
 
-app.get('/', (req, res) => {
-    res.json("I love docker!");
-});
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    console.log('Your server is running on PORT:',PORT);
-});
+routes(app);
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
